685245fd8b7ce3813a6a8e29f5260d54647e8bbf
Mille Loo <74830754+Pinkvinus@users.noreply.github.com>
2023-06-28 00:11:19 +0200

    Yarn (#1)
    * added YarnInfo enums
    * added yarn class, fields, constructo

---
+++ Model/YarnInfo.cs
@@ -0,0 +1,31 @@
+enum YarnWeights
+{
+    superfine, //supertype - Superfine
+    baby,
+    fingering,
+    sock,
+    fine,      //supertype - Fine
+    sport,
+    light,     //supertype - Light
+    dk,
+    worsted,
+    medium,    //supertype - Medium
+    afghan,
+    aran,
+    bulky,     //supertype - Bulky
+    chunky,
+    craft,
+    rug,
+    superbulky, //supertype - Super bulky
+    roving,
+    jumbo      //supertype - jumbo
+}
+
+enum YarnMaterial
+{
+    wool,
+    cotton,
+    alpaca,
+    bamboo,
+    mohair,
+}
