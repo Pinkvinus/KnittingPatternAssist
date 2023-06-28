d8c14b0ef417bab3b1cfb2662a6c0f4ae0a003cc
Mille Mei Zhen Loo <milo@itu.dk>
2023-06-28 11:57:39 +0200

    Rebased the project to Maui

---
+++ KnittingPatternCreator/KnittingPatternCreator/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace KnittingPatternCreator;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ KnittingPatternCreator/KnittingPatternCreator/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace KnittingPatternCreator;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
